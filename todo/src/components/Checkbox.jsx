01e94ce12a38955f5222d72c18fdd63658cba3ea
snoxrow <105754271+snoxrow@users.noreply.github.com>
2023-03-15 23:47:17 +0100

    Small type error fixes

--- todo/src/components/Checkbox.jsx
+++ todo/src/components/Checkbox.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Checkbox = ({onCheck, task}) => {
+const Checkbox = ({ onCheck, task }) => {
   return (
-    <div className='checkbox'><input id={task.id} type="checkbox" defaultChecked= {false} checked={task.checked} onClick={() => onCheck(task.id)}/>
-    <label for={task.id} ></label></div>
-  )
-}
+    <div className="checkbox">
+      <input
+        id={task.id}
+        type="checkbox"
+        // defaultChecked={false}
+        checked={task.checked}
+        onChange={() => onCheck(task.id)}
+      />
+      <label htmlFor={task.id}></label>
+    </div>
+  );
+};
 
-export default Checkbox
+export default Checkbox;
